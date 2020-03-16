f129b63031eb4091b583120f2d32fdbf5ff93735
Jovani Martinez <armando.martinez.1738@gmail.com>
2020-03-16 16:16:35 -0600

    Botón comprar
    Botón comprar en los productos

--- Armando Jovani Martinez Ruiz/ecommerce/php/componentes/contenido.php
+++ Armando Jovani Martinez Ruiz/ecommerce/php/componentes/contenido.php
@@ -2,78 +2,44 @@
 
    <div class="main" id="contenido">
             <section class="products">
-                <article class="card" style="width: 15rem; height: 25rem; ">
+                <article class="card" style="width: 15rem; height: 30rem; ">
                     <img src="https://via.placeholder.com/250x250" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h4 class="card-title">Nike 950</h4>
                         <p class="price"> $ 3900</p>
                         <p class="card-text">Calzado de runnig para hombre.</p>
+                        <button type="button" class="btn btn-success" >Comprar</button>
                     </div>
                 </article>
-                <article class="card" style="width: 15rem; height: 25rem; ">
+                <article class="card" style="width: 15rem; height: 30rem; ">
                     <img src="https://via.placeholder.com/250x250" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h4 class="card-title">Nike 950</h4>
                         <p class="price"> $ 3900</p>
                         <p class="card-text">Calzado de runnig para hombre.</p>
+                        <button type="button" class="btn btn-success" >Comprar</button>
                     </div>
                 </article>
-                <article class="card" style="width: 15rem; height: 25rem; ">
+                <article class="card" style="width: 15rem; height: 30rem; ">
                     <img src="https://via.placeholder.com/250x250" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h4 class="card-title">Nike 950</h4>
                         <p class="price"> $ 3900</p>
                         <p class="card-text">Calzado de runnig para hombre.</p>
+                        <button type="button" class="btn btn-success" >Comprar</button>
                     </div>
                 </article>
-                <article class="card" style="width: 15rem; height: 25rem; ">
+                <article class="card" style="width: 15rem; height: 30rem; ">
                     <img src="https://via.placeholder.com/250x250" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h4 class="card-title">Nike 950</h4>
                         <p class="price"> $ 3900</p>
                         <p class="card-text">Calzado de runnig para hombre.</p>
+                        <button type="button" class="btn btn-success" >Comprar</button>
                     </div>
                 </article>
-                <article class="card" style="width: 15rem; height: 25rem; ">
-                    <img src="https://via.placeholder.com/250x250" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h4 class="card-title">Nike 950</h4>
-                        <p class="price"> $ 3900</p>
-                        <p class="card-text">Calzado de runnig para hombre.</p>
-                    </div>
-                </article>
-                <article class="card" style="width: 15rem; height: 25rem; ">
-                    <img src="https://via.placeholder.com/250x250" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h4 class="card-title">Nike 950</h4>
-                        <p class="price"> $ 3900</p>
-                        <p class="card-text">Calzado de runnig para hombre.</p>
-                    </div>
-                </article>
-                <article class="card" style="width: 15rem; height: 25rem; ">
-                    <img src="https://via.placeholder.com/250x250" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h4 class="card-title">Nike 950</h4>
-                        <p class="price"> $ 3900</p>
-                        <p class="card-text">Calzado de runnig para hombre.</p>
-                    </div>
-                </article>
-                <article class="card" style="width: 15rem; height: 25rem; ">
-                    <img src="https://via.placeholder.com/250x250" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h4 class="card-title">Nike 950</h4>
-                        <p class="price"> $ 3900</p>
-                        <p class="card-text">Calzado de runnig para hombre.</p>
-                    </div>
-                </article>
-                <article class="card" style="width: 15rem; height: 25rem; ">
-                    <img src="https://via.placeholder.com/250x250" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h4 class="card-title">Nike 950</h4>
-                        <p class="price"> $ 3900</p>
-                        <p class="card-text">Calzado de runnig para hombre.</p>
-                    </div>
-                </article>
+        
+               
 
             </section>
             <!-- Fin productos -->
